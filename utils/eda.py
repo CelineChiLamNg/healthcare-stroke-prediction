4bbeffd1168cec4e5a76e36440b647031916d9d4
celineng <celine86yan@gmail.com>
2024-09-18 15:47:15 +0100

    plot

--- utils/eda.py
+++ utils/eda.py
@@ -9,8 +9,23 @@ from typing import Optional, List
 
 def missing_values(df: pd.DataFrame) -> pd.DataFrame:
     length = df.shape[0]
-    missing_values = df.isnull().sum().reset_index()
-    missing_values.columns = ['features', 'null_count']
-    missing_values['null_%'] = (missing_values['null_count']*100 /
-                                      length).round(2)
-    return missing_values
+    missing_values_count = df.isnull().sum()
+    missing_values_percentage = (df.isnull().sum() / len(df)) * 100).round(2)
+    summary_df = pd.DataFrame({
+        'Column': df.columns,
+        'Missing Values': missing_values_count,
+        'Percentage Missing': missing_values_percentage
+    })
+    return summary_df
+
+def violin_boxplot(data: pd.DataFrame, columns: List[str], title: Optional[
+    str] = None) -> plt.Figure:
+    plt.figure(figsize=(10, 6))
+    sns.violinplot(data=data[columns], orient='h',
+                   density_norm='count', inner=None)
+    sns.boxplot(data=data[columns], width=0.2,
+                showfliers=True,
+                boxprops={'facecolor': 'None'}, orient='h')
+    if title:
+        plt.title(title)
+    plt.show()
